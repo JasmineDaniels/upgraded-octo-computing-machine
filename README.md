621e0b1cc020962e3c8437ef6832d155936854b8
Jasmine Daniels <create.jasminedaniels@gmail.com>
2024-08-16 15:56:51 -0400

    starter script

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# upgraded-octo-computing-machine
+# Essbase Client Wallet Rotation
 Script to automate Essbase Client Wallet Rotation
+
+
+### Needs:
+- Test Environment 
+        - [Essbase Documentation for Testing with Swagger](https://docs.oracle.com/en/database/other-databases/essbase/21/essrt/quick-start.html) 
+        - Mock Environment (Andy/Brad)
+
+### Questions:
+
+- Authentication for user/pw (.bat file)
---
+++ walletRotation.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#Essbase Client Wallet Rotation Script
+
+DATE=`date +%m%d%y`
+WALLET_ZIP_FILE=''
+CLIENT_NAME=''
+CONNECTION_NAME=''
+WALLET_DIR='' 
+
+#TODO: Get Wallet Files
+
+#TODO: Update Zip file permissions
+
+#TODO: Test Updated Connection Dry Run: 
+#Documentation https://docs.oracle.com/en/database/other-databases/essbase/21/essrt/op-connections-actions-test-post.html
+#create json (EOF) with service and walletPath properties
+test_connection(){
+
+}
+
+#Upload Global Connection Wallet File
+# **MUST CLARIFY AUTHENTICATION** via properties.bat file or some other method..
+upload_wallet(){
+    echo "Uploading Wallet..."
+    curl -X PUT "https://myserver.example.com:9001/essbase/rest/v1/connections/$CONNECTION_NAME/wallet" -H Accept:application/json -H Content-Type:application/octet-stream --data-binary "@./dbwallet.zip" -u %User%:%Password%
+    if [[ $? -ne 0 ]]; then
+        echo "Error: Failed to Update wallet."
+        exit 1
+    fi
+
+    #Check Service Name
+    # ** check if service name is set to medium**
+    response=$(curl -X GET "https://myserver.example.com:9001/essbase/rest/v1/connections/$CONNECTION_NAME?links=none" -H Accept:application/json -u %User%:%Password%)
+    service=$(echo $response | jq ".service")
+    if [[ $service == *"medium"* ]]; then
+        echo "Service name is set to medium."
+    fi
+}
+
+
+#Test Connection
+# **MUST CLARIFY AUTHENTICATION** via properties.bat file or some other method..
+final_connection_test(){
+    echo "Testing New Connection..."
+    curl -X POST -i "https://myserver.example.com:9001/essbase/rest/v1/connections/$CONNECTION_NAME/actions/test" -H Accept:application/json -u %User%:%Password%
+
+    if [[ $? -ne 0 ]]; then
+        echo "Error occurred while testing connection."
+        exit 1
+    fi
+}
