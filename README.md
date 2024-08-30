49d80ad8cfc171c9d6f77fea14b0f137c6b84942
Jasmine Daniels <create.jasminedaniels@gmail.com>
2024-08-29 20:20:47 -0400

    finish dry run test, rotate all connections --wip

--- README.md
+++ README.md
@@ -5,8 +5,9 @@ Script to automate Essbase Client Wallet Rotation
 ### Needs:
 - Test Environment 
         - [Essbase Documentation for Testing with Swagger](https://docs.oracle.com/en/database/other-databases/essbase/21/essrt/quick-start.html) 
-        - Mock Environment (Andy/Brad)
+- Mock Environment (Andy/Brad)
 
 ### Questions:
 
 - Authentication for user/pw (.bat file)
+- Type of Connection 
--- walletRotation.sh
+++ walletRotation.sh
@@ -2,27 +2,43 @@
 #Essbase Client Wallet Rotation Script
 
 DATE=`date +%m%d%y`
-WALLET_ZIP_FILE=''
-CLIENT_NAME=''
-CONNECTION_NAME=''
 WALLET_DIR='' 
+WALLET_ZIP_FILE=""
 
 #TODO: Get Wallet Files
 
 #TODO: Update Zip file permissions
 
-#TODO: Test Updated Connection Dry Run: 
-#Documentation https://docs.oracle.com/en/database/other-databases/essbase/21/essrt/op-connections-actions-test-post.html
-#create json (EOF) with service and walletPath properties
-test_connection(){
+#TODO: **MUST CLARIFY AUTHENTICATION** 
 
+#Test Updated Connection Dry Run: 
+test_connection(){
+echo "Testing Connection" 
+CONNECTION_NAME=$1
+cat << EOF > conn_details.json
+{
+    "name" : "$CONNECTION_NAME",
+    "type" : "DB",
+    "subtype" : "ORACLE",
+    "walletPath" : $WALLET_DIR,
+    "repoWallet" : "false",
+    "user" : "",
+    "password" : "",
+    "service" : ""
+}
+EOF
+curl -X POST -i "https://myserver.example.com:9001/essbase/rest/v1/connections/actions/test" -H Accept:application/json -H Content-Type:application/json --data "conn_details.json" -u %User%:%Password%
+if [[ $? -ne 0 ]]; then
+    echo "Error: Failed to Test Connection."
+    exit 1
+fi
 }
 
-#Upload Global Connection Wallet File
-# **MUST CLARIFY AUTHENTICATION** via properties.bat file or some other method..
+#Upload Global Connection Wallet File 
 upload_wallet(){
     echo "Uploading Wallet..."
-    curl -X PUT "https://myserver.example.com:9001/essbase/rest/v1/connections/$CONNECTION_NAME/wallet" -H Accept:application/json -H Content-Type:application/octet-stream --data-binary "@./dbwallet.zip" -u %User%:%Password%
+    CONNECTION_NAME=$1
+    curl -X PUT "https://myserver.example.com:9001/essbase/rest/v1/connections/$CONNECTION_NAME/wallet" -H Accept:application/json -H Content-Type:application/octet-stream --data-binary $WALLET_ZIP_FILE -u %User%:%Password%
     if [[ $? -ne 0 ]]; then
         echo "Error: Failed to Update wallet."
         exit 1
@@ -34,14 +50,15 @@ upload_wallet(){
     service=$(echo $response | jq ".service")
     if [[ $service == *"medium"* ]]; then
         echo "Service name is set to medium."
+        #TODO: Add Else Case
     fi
 }
 
 
 #Test Connection
-# **MUST CLARIFY AUTHENTICATION** via properties.bat file or some other method..
 final_connection_test(){
     echo "Testing New Connection..."
+    CONNECTION_NAME=$1
     curl -X POST -i "https://myserver.example.com:9001/essbase/rest/v1/connections/$CONNECTION_NAME/actions/test" -H Accept:application/json -u %User%:%Password%
 
     if [[ $? -ne 0 ]]; then
@@ -49,3 +66,16 @@ final_connection_test(){
         exit 1
     fi
 }
+
+#List Connections
+list_connections(){
+    connections=($(curl -X GET "https://myserver.example.com:9001/essbase/rest/v1/connections?links=none" -H Accept:application/json -u %User%:%Password% | jq -r ".[].name"))
+    for connection_name in ${connections[*]}
+    do 
+    test_connection $connection_name
+    upload_wallet $connection_name
+    final_connection_test $connection_name
+    done
+}
+
+list_connections
